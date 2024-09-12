0940549dc241a05c16bfb4c1b61ccacb12176975
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2024-09-12 10:43:30 +0700

    add file env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
+// console.log(process.env.DB_URL);
+
 const connectDb = () => {
-    mongoose.connect('mongodb://localhost:27017/checkCc')
+    mongoose.connect(process.env.DB_URL ?? "")
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 } 
